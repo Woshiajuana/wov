0b71876e399ed3e8efda82aace5f9b68d0b91153
Woshiajuana <zhigang.chen@owulia.com>
2017-09-29 18:08:17 +0800

    更新

---
+++ wov-demo/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+    <wov-switch
+            :switch_page_arr="switch_page_arr">
+
+    </wov-switch>
+</template>
+
+<script>
+    import { WovSwitch } from 'wov'
+    export default {
+        data () {
+            return {
+                switch_page_arr: [{
+                    txt: '首页1',
+//                    src: PageUrlConfig['message'],
+                    visibility: 'visible',
+                    img_icon_name: 'home-icon',
+                    checked: true
+                },
+                {
+                    txt: '发现1',
+//                    src: PageUrlConfig['find'],
+                    visibility: 'hidden',
+                    img_icon_name: 'find-icon',
+                    checked: false
+                },
+                {
+                    txt: '留言',
+//                    src: PageUrlConfig['home'],
+                    visibility: 'hidden',
+                    img_icon_name: 'message-icon',
+                    checked: false
+                },
+                {
+                    txt: '关于',
+//                    src: PageUrlConfig['about'],
+                    visibility: 'hidden',
+                    img_icon_name: 'about-icon',
+                    checked: false
+                }]
+            }
+        },
+        methods: {
+            /**切换导航菜单*/
+            switchMenuHandle (index) {
+                for (var i = 0; i < this.switch_page_arr.length; i++) {
+                    var item = this.switch_page_arr[i];
+                    if (i == index) {
+                        item.visibility = 'visible';
+                        item.checked = true;
+                    } else {
+                        item.visibility = 'hidden';
+                        item.checked = false;
+                    }
+                }
+            }
+        },
+        components: {
+            WovSwitch
+        }
+    }
+</script>
+
+<style scoped>
+    .menu-content{
+        flex: 1;
+    }
+</style>
