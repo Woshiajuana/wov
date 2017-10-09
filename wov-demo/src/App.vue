647544b8dd7c8082c3d3e550499f87d0a69e5d01
Woshiajuana <zhigang.chen@owulia.com>
2017-10-09 10:21:26 +0800

    更新

--- wov-demo/src/App.vue
+++ wov-demo/src/App.vue
@@ -6,23 +6,27 @@
 
 <script>
     import { WovSwitch } from 'wov'
+    import PageUrlConfig from './config/page_url_config'
     export default {
         data () {
             return {
                 switch_page_arr: [{
                     txt: 'HOME',
+                    src: PageUrlConfig['home'],
                     visibility: 'visible',
                     img_icon_name: 'home-icon',
                     checked: true
                 },
                 {
                     txt: 'DEMO',
+                    src: PageUrlConfig['home'],
                     visibility: 'hidden',
                     img_icon_name: 'find-icon',
                     checked: false
                 },
                 {
                     txt: 'ABOUT',
+                    src: PageUrlConfig['home'],
                     visibility: 'hidden',
                     img_icon_name: 'message-icon',
                     checked: false
