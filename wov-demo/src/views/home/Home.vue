0f0b4476736418f764d625619bf9c3bab571115a
Woshiajuana <zhigang.chen@owulia.com>
2017-10-10 13:11:15 +0800

    更新

--- wov-demo/src/views/home/Home.vue
+++ wov-demo/src/views/home/Home.vue
@@ -3,176 +3,29 @@
         :is_header="false"
         :is_refresh="true"
         :is_load="true"
-        background_color="red"
         @refresh="refreshHandle"
         @load="loadHandle"
         @pullingDown="pullingDownHandle"
         @pullingUp="pullingUpHandle"
         ref="scroller">
-        <div class="home-header">
-            <text @click="onClick">点击我改变</text>
-            <text @click="onClick" v-if="type">隐藏我</text>
-            <text @click="onClick" v-if="!type">显示我</text>
-            <input v-model="value_input"/>
-            <text>{{value_input}}</text>
-            <div @click="getVe">
-                <text class="code" v-if="verify_send">获取验证码</text>
-            </div>
-            <div>
-                <text class="code-count" v-if="!verify_send">{{rows}}秒</text>
-            </div>
-        </div>
-        <article-list-item v-for="item in rows" :key="item"></article-list-item>
+        <image src=""></image>
     </wov-view>
 </template>
 
 <script>
     import { WovView } from 'wov'
-    import ArticleListItem from '../../components/article-list-item.vue'
     export default {
         data () {
             return {
-                rows: 10,
-                type: true,
-                value_input: '',
-                verify_send: true
-            }
-        },
-        watch: {
 
-        },
-        computed: {
-            /**日期计算属性*/
-            dateComputed () {
-                return Tool.format('yyyy-MM-dd');
-            },
-            /**几号计算属性*/
-            dayComputed () {
-                return Tool.format('dd');
-            }
-        },
-        created(){
-            console.log(1)
-        },
-        methods: {
-            getVe () {
-                this.verify_send = !this.verify_send;
-            },
-            onClick () {
-                this.type = !this.type;
-            },
-            /**下拉距离*/
-            pullingDownHandle (event) {
-            },
-            /**上拉距离*/
-            pullingUpHandle (event) {
-            },
-            refreshHandle () {
-                setTimeout(() => {
-                    this.rows = 10;
-                    this.$refs.scroller.refreshed();
-                    this.$refs.scroller.regainLoad();
-                }, 2000)
-            },
-            loadHandle () {
-                setTimeout(() => {
-                    this.rows += 10;
-                    if(this.rows >= 30) {
-                        this.$refs.scroller.banLoad();
-                    }
-                    this.$refs.scroller.loaded();
-                }, 1500)
-            },
-            toSearchHandle () {
-                navigator.push({url: PageUrl['search']})
-            },
-            filterHandle () {
-                navigator.push({url: PageUrl['filter']})
             }
         },
         components: {
-            ArticleListItem,
             WovView
         }
     }
 </script>
 
 <style>
-    .home-header{
-        width: 750px;
-        height: 240px;
-        padding-top: 20px;
-        padding-bottom: 20px;
-        background-color: #58B7FF;
-    }
-    .home-header-search-link{
-        background-color: #fff;
-        margin-top: 20px;
-        margin-left: 32px;
-        margin-right: 32px;
-        height: 72px;
-        border-radius: 72px;
-        box-shadow: 0 6px 6px 0 rgba(0,0,0,.2);
-    }
-    .home-header-search-link-txt{
-        text-align: center;
-        color: #333;
-        font-size: 28px;
-        line-height: 72px;
-    }
-    .home-header-top{
-        position: relative;
-        color: #f2f2f2;
-        height: 70px;
-        line-height: 70px;
-    }
-    .home-header-top-date{
-        position: absolute;
-        top: 0;
-        height: 70px;
-        left: 40px;
-        padding-left: 60px;
-    }
-    .home-header-top-date:after{
-        position: absolute;
-        right: -30px;
-        content: '';
-        width: 0;
-        height: 0;
-        border-left: 5px solid transparent;
-        border-right: 5px solid transparent;
-        border-top:  6px solid #f2f2f2;
-    }
-    .home-header-top-date-con{
-        position: absolute;
-        left: 0;
-        top: 13px;
-        height: 44px;
-        width: 44px;
-    }
-    .home-header-top-date-con-day-txt{
-        text-align: center;
-        line-height: 60px;
-        color: #fff;
-        font-size: 22px;
-    }
-    .home-header-top-date-txt{
-        line-height: 70px;
-        font-size: 28px;
-        color: #fff;
-    }
-    .home-header-date-icon{
-        position: absolute;
-        height: 48px;
-        width: 48px;
-        left: 0;
-        top: 0;
-    }
-    .home-header-top-filter-btn{
-        position: absolute;
-        height: 50px;
-        width: 50px;
-        top: 10px;
-        right: 40px;
-    }
+
 </style>
--- wov-demo/src/views/register.vue
+++ wov-demo/src/views/register.vue
@@ -1,92 +1,98 @@
 <template>
-  <div class="register_wrap">
-      <!--<div class="register_title">-->
-          <!--<image class="back_icon" @click="close" :src="back_btn"></image>-->
-          <!--<text class="text_title">注册</text>-->
-      <!--</div>-->
-      <!--<div class="register_input">-->
-          <!--<image class="img_icon" :src="login_phone"></image>-->
-          <!--<input ref="input_num" maxlength="11" type="text" class="input" :value="inputvalue"  placeholder="请输入手机号码" @change="change" @input="input_phoneNum" @focus="focus"/>-->
-          <!--<image class="hide_del" v-if="hide_del_num" @click="del_num" :src="hide_del"></image>-->
-      <!--</div>-->
-      <!--<div class="register_input">-->
-          <!--<image class="img_icon_special" :src="register_code"></image>-->
-          <!--<input maxlength="6" type="text"  class="input" :value="code_value"  placeholder="短信验证码" @input="input_password"/>-->
-          <!--<image class="img_border" :src="img_border"></image>-->
-          <!--<text class="code" v-if="type">获取验证码</text>-->
-      <!--</div>-->
-      <div class="register_agree">
-          <text class="agree_text" @click="onClick">点击下一步，即表示已阅读并同意1</text>
-          <!--<text class="agree" @click="agree">《优米服务协议》</text>-->
-      </div>
-      <!--<div class="register_btn">-->
-          <!--<text class="primary_btn" v-if="fullInfo">下一步</text>-->
-          <!--<text class="fullInfo_btn" v-else @click="login">下一步</text>-->
-      <!--</div>-->
-      <!--<div class="noregister">-->
-          <!--<text class="noregister_text">我不想注册，先看看再说</text>-->
-      <!--</div>-->
-      <text @click="onClick">点击我改变</text>
-      <text @click="onClick" v-if="type">隐藏我</text>
-      <text @click="onClick" v-if="!type">显示我</text>
-  </div>
+    <div class="register_wrap">
+        <!--<div class="register_title">-->
+        <!--<image class="back_icon" @click="close" :src="back_btn"></image>-->
+        <!--<text class="text_title">注册</text>-->
+        <!--</div>-->
+        <!--<div class="register_input">-->
+        <!--<image class="img_icon" :src="login_phone"></image>-->
+        <!--<input ref="input_num" maxlength="11" type="text" class="input" :value="inputvalue"  placeholder="请输入手机号码" @change="change" @input="input_phoneNum" @focus="focus"/>-->
+        <!--<image class="hide_del" v-if="hide_del_num" @click="del_num" :src="hide_del"></image>-->
+        <!--</div>-->
+        <!--<div class="register_input">-->
+        <!--<image class="img_icon_special" :src="register_code"></image>-->
+        <!--<input maxlength="6" type="text"  class="input" :value="code_value"  placeholder="短信验证码" @input="input_password"/>-->
+        <!--<image class="img_border" :src="img_border"></image>-->
+        <!--<text class="code" v-if="type">获取验证码</text>-->
+        <!--</div>-->
+        <div class="register_agree">
+            <text class="agree_text" @click="onClick">点击下一步，即表示已阅读并同意1</text>
+            <text>{{count}}</text>
+            <!--<text class="agree" @click="agree">《优米服务协议》</text>-->
+        </div>
+        <!--<div class="register_btn">-->
+        <!--<text class="primary_btn" v-if="fullInfo">下一步</text>-->
+        <!--<text class="fullInfo_btn" v-else @click="login">下一步</text>-->
+        <!--</div>-->
+        <!--<div class="noregister">-->
+        <!--<text class="noregister_text">我不想注册，先看看再说</text>-->
+        <!--</div>-->
+        <text @click="onClick">点击我改变</text>
+        <text @click="onClick" v-if="type">隐藏我</text>
+        <text @click="onClick" v-if="!type">显示我</text>
+    </div>
 </template>
 
 <script>
-//import source from '../util/source.js'
-const navigator=weex.requireModule('navigator')
-const source = function () {
-    return ''
-}
-export default {
-  data(){
-    return{
-      hide_del_num:false,
-      inputvalue:'',
-      code_value:'',
-      fullInfo:false,
-        type: true
+    //import source from '../util/source.js'
+    const navigator=weex.requireModule('navigator')
+    const source = function () {
+        return ''
     }
-  },
-  computed:{
-    back_btn:function(){
-      return source('nav_back.png')
-    },
-    hide_del:function(){
-      return source('logo_clear.png')
-    },
-    login_phone:function(){
-      return source('logo_icon_phone.png')
-    },
-    register_code:function(){
-      return source('register_icon_password.png')
-    },
-    img_border:function(){
-      return source('my_line.png')
-    },
-  },
-  methods:{
-      onClick() {
-          this.type = !this.type;
-      },
-    input_phoneNum(e){
-      this.inputvalue=e.value;
-      if(this.inputvalue!=''){
-        this.hide_del_num=true;
-      }else{
-        this.hide_del_num=false;
-      }
-      if(this.inputvalue!=='' && this.code_value!==''){
-        this.fullInfo=false
-      }
-    },
-    close(){
-      navigator.pop({
-        animated:true
-      },callback=>{})
+    export default {
+        data(){
+            return{
+                hide_del_num:false,
+                inputvalue:'',
+                code_value:'',
+                fullInfo:false,
+                type: true,
+                count: 60,
+                timer: ''
+            }
+        },
+        computed:{
+            back_btn:function(){
+                return source('nav_back.png')
+            },
+            hide_del:function(){
+                return source('logo_clear.png')
+            },
+            login_phone:function(){
+                return source('logo_icon_phone.png')
+            },
+            register_code:function(){
+                return source('register_icon_password.png')
+            },
+            img_border:function(){
+                return source('my_line.png')
+            },
+        },
+        methods:{
+            onClick() {
+                this.type = !this.type;
+                this.timer = setInterval(() => {
+                    this.count--;
+                },1000)
+            },
+            input_phoneNum(e){
+                this.inputvalue=e.value;
+                if(this.inputvalue!=''){
+                    this.hide_del_num=true;
+                }else{
+                    this.hide_del_num=false;
+                }
+                if(this.inputvalue!=='' && this.code_value!==''){
+                    this.fullInfo=false
+                }
+            },
+            close(){
+                navigator.pop({
+                    animated:true
+                },callback=>{})
+            }
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
