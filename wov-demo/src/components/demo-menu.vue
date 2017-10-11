352dfaaf5c8e8d7b2c41a40faceb1acf6f1acf12
Woshiajuana <zhigang.chen@owulia.com>
2017-10-11 10:59:05 +0800

    更新

---
+++ wov-demo/src/components/demo-menu.vue
@@ -0,0 +1,58 @@
+<template>
+    <wov-view
+        class="view"
+        center_item_title="WovSwitch"
+        :left_item_img_src="left_item_img_src">
+        <div class="main-wrap">
+            <wov-item
+                v-for="(item, index) in menu_arr"
+                left_width='200'
+                height="100"
+                :key="index"
+                :left_text="item.left_text"
+                :border_bottom_width="index == menu_arr.length - 1 ? 0 : 2"
+                :left_img_width_and_height="[60,60]"
+                @onPress="onPressHandle(item)">
+            </wov-item>
+        </div>
+    </wov-view>
+</template>
+
+<script>
+    import { WovView, WovItem } from 'wov'
+    import source from '../assets/lib/source'
+    export default {
+        data () {
+            return {
+                left_item_img_src: source('return-icon.png')
+            }
+        },
+        props: {
+            menu_arr: { default: '' }
+        },
+        methods: {
+            onPressHandle ( item ) {
+                this.$emit('onPress', item);
+            }
+        },
+        components: {
+            WovView,
+            WovItem
+        }
+    }
+</script>
+
+<style>
+    .view{
+        background-color: #fbf9fe;
+    }
+    .main-wrap{
+        margin-top: 180px;
+        border-top-color: #ddd;
+        border-top-width: 2px;
+        border-top-style: solid;
+        border-bottom-color: #ddd;
+        border-bottom-width: 2px;
+        border-bottom-style: solid;
+    }
+</style>
--- wov-demo/src/views/switch/index.vue
+++ wov-demo/src/views/switch/index.vue
@@ -1,45 +1,17 @@
 <template>
-    <wov-view
-        class="view"
-        :left_item_img_src="icon"
-        center_item_title="WovSwitch">
-        <div class="main-wrap">
-            <wov-item
-                left_text="居顶的TAB切换"
-                left_width='200'
-                height="100"
-                border_bottom_width="2"
-                :left_img_width_and_height="[60,60]"
-                @onPress="onPressHandle('Official')">
-            </wov-item>
-            <wov-item
-                left_text="居底的TAB切换"
-                height="100"
-                border_bottom_width="2"
-                :left_img_width_and_height="[60,60]"
-                @onPress="onPressHandle('Official')">
-            </wov-item>
-            <wov-item
-                left_text="APP菜单"
-                height="100"
-                border_bottom_width="0"
-                :left_img_width_and_height="[60,60]"
-                @onPress="onPressHandle('Official')">
-            </wov-item>
-        </div>
-    </wov-view>
+    <demo-menu :menu_arr="menu_arr"></demo-menu>
 </template>
 
 <script>
-    import { WovView, WovItem } from 'wov'
-    import source from '../../assets/lib/source'
+    import DemoMenu from '../../components/demo-menu.vue'
     export default {
         data () {
             return {
-                official_img_src: source('official-icon.png'),
-                arrow_img_src: source('right-arrow-icon.png'),
-                left: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAf1JREFUWAntmL0rRlEcx72EWEjKS0oUKRZZTAZllEUWI4NBmQwmJhkMLAZZGBgMKCv5AwyKEkVKSSjvkffPt/x0O930yNNzTnp+9e2c873POb/P/d3z3HufJyMjHekK/I8KFHAai+gOraJ8FEwIZh19RDSQLLqsPy6Ux/wV1OasI1jvkQvBGopWTv0t5P0S5wChyrlw23jFyGtkk30JuXA7eCVeyUiuPbuAXLg9vFLkNQQ3h1y4A7xyr2Qkz0SzyIU7xKtE3mMaAhfuGK/KOxkAU8iFO8GrCQFuIgbuFK82BLixGLgzvPoQ4EZj4C7wGlINp2+nG4MYk46pPfj25bn7UWOF6yfqRee/MGkT9aMHFBt6ZXKTpXo8bGS6+bpx4xoexoU/5Wzm4DVKddUs3y65Kwwwbg/qWBHaQE0aRGKZfg/SftRcE93v/k+e5bPPWGvzte6lBomE3kx0NnZm1s7jxW2NRNZM+mfKWHEfGZy1M3hWjaQn/e2Cehk4QgZnrR6BwUQ1JHr+Gpy148EQAlKH9KgzOGtHQoJsBEbfMoOzdigkSN16rmIgB0KCbAHm1oF8Z9wbEmQrMHqg22VWqxutbuTBRDskTygK+cq4MxhCQDrQM4pCnocEKJYupMoZ5D19/cAPKrqh0VvQI+oLiiwCo6rpf5t0pCuQaAU+AQjE6XRPS3R6AAAAAElFTkSuQmCC',
-                icon: source('return-icon.png')
+                menu_arr: [
+                    { left_text: '居顶的TAB切换', page_name: '' },
+                    { left_text: '居底的TAB切换', page_name: '' },
+                    { left_text: 'APP菜单切换', page_name: '' },
+                ]
             }
         },
         methods: {
@@ -48,23 +20,7 @@
             }
         },
         components: {
-            WovView,
-            WovItem
+            DemoMenu
         }
     }
 </script>
-
-<style>
-    .view{
-        background-color: #fbf9fe;
-    }
-    .main-wrap{
-        margin-top: 180px;
-        border-top-color: #ddd;
-        border-top-width: 2px;
-        border-top-style: solid;
-        border-bottom-color: #ddd;
-        border-bottom-width: 2px;
-        border-bottom-style: solid;
-    }
-</style>
