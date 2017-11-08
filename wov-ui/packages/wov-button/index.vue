fb4be6db9ef917e49ff8e43939741d29c206944d
Woshiajuana <zhigang.chen@owulia.com>
2017-11-08 12:41:40 +0800

    优化

---
+++ wov-ui/packages/wov-button/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div @click="pressHandle">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        name: 'wov-button',
+        props: {
+            disabled: { default: false },
+            delay: { default: 0 }
+        },
+        methods: {
+            pressHandle () {
+                !this.disabled && this.$emit('onPress', ()=>{
+                    setTimeout(() => {
+                        this.disabled = false;
+                    }, +this.delay);
+                });
+                this.disabled = true;
+            }
+        }
+    }
+</script>
