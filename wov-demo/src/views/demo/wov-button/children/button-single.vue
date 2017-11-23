a84a5da51ce8616093d89bebfc5c7c1614feb3be
Woshiajuana <zhigang.chen@owulia.com>
2017-11-23 18:05:40 +0800

    更新组件

--- wov-demo/src/views/demo/wov-button/children/button-single.vue
+++ wov-demo/src/views/demo/wov-button/children/button-single.vue
@@ -1,7 +1,13 @@
 <template>
     <wov-view view_header_center_txt="过滤连续点击">
-        <wov-button @onPress="onPressHandle(1, $event)">
+        <wov-button @onPress="oneHandle">
             <wov-cell cell_left_txt="点击一下，就不可点击"></wov-cell>
+        </wov-button>
+        <wov-button @onPress="twoHandle">
+            <wov-cell cell_left_txt="始终都可以点击"></wov-cell>
+        </wov-button>
+        <wov-button @onPress="threeHandle">
+            <wov-cell cell_left_txt="3秒之后才能再次点击" :cell_right_txt="time_prompt"></wov-cell>
         </wov-button>
     </wov-view>
 </template>
@@ -10,20 +16,29 @@
     import WovCell from '../../../../components/wov/packages/wov-cell'
     import WovButton from '../../../../components/wov/packages/wov-button'
     import source from '../../../../assets/lib/source'
+    import dialogs from '../../../../module/dialogs'
     export default {
         data () {
             return {
                 img_src: source('cell-icon.png'),
-                type
+                time_prompt: '可点击'
             }
         },
         methods: {
-            onPressHandle (type, callback) {
-                switch (type) {
-                    case 1:
-
-                        break;
-                }
+            oneHandle (callback) {
+                dialogs.toast('你点击了一下,就不能再点击了');
+            },
+            twoHandle (callback) {
+                callback();
+                dialogs.toast('你始终都可以点击哦');
+            },
+            threeHandle (callback) {
+                this.time_prompt = '不能点击';
+                setTimeout(() => {
+                    callback();
+                    this.time_prompt = '可点击';
+                },3000);
+                dialogs.toast('3秒之后才能再次点击');
             }
         },
         components: {
